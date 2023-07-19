1732593dfbb35915f03673332e1a869f4b0506b8
Phạm Thành Vinh <thanhvinh07032003@gmail.com>
2023-07-19 11:33:58 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./views/Nav";
+import { useState } from "react";
 
-function App() {
+//~~class
+const App = () => {
+  //state
+  let [name, setName] = useState("Vinh");
+  let [address, setAddress] = useState("");
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
+        <h3>Hello world with {name}</h3>
+        <h4>Address: {address}</h4>
+        <input
+          type="text"
+          value={address}
+          onChange={(event) => {
+            setAddress(event.target.value);
+          }}
+        />
+        <button
+          type="button"
+          style={{ marginTop: "10px" }}
+          onClick={() => {
+            setName(address);
+          }}
+        >
+          Click me
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
