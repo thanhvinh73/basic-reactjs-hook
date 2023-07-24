2c2ed812e2fa1a2f4120ef251efc1e7994b7b6c2
Phạm Thành Vinh <thanhvinh07032003@gmail.com>
2023-07-24 23:54:29 +0700

    Reusing Hook

--- src/views/BlogDetail.js
+++ src/views/BlogDetail.js
@@ -1,20 +1,42 @@
 import { useNavigate, useParams } from "react-router-dom";
-
+import useFetch from "../customize/fetch";
+import "./Blog.scss";
 const BlogDetail = () => {
   const { id } = useParams();
   let navigate = useNavigate();
+
+  const {
+    data: blogDetail,
+    isLoading,
+    isError,
+  } = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
   return (
     <>
-      <div>
-        <span
-          onClick={() => {
-            navigate("/blog");
-          }}
-        >
-          {`<--`} Back
-        </span>{" "}
+      <div className="blog-detail">
+        {blogDetail && (
+          <>
+            <div className="blog-id"> Blog ID: {id} </div>
+            {isLoading ? (
+              <div>Loading data...</div>
+            ) : (
+              <>
+                <div className="title">{blogDetail.title}</div>
+                <div className="content">{blogDetail.body}</div>
+                <button
+                  className="button-back"
+                  type="button"
+                  onClick={() => {
+                    navigate("/blog");
+                  }}
+                >
+                  Back
+                </button>
+              </>
+            )}
+          </>
+        )}
       </div>
-      <div>Hello from blog detail with id: {id}</div>
     </>
   );
 };
